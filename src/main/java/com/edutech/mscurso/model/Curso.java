9836d91675ccff6d460ddc6c1c1ccc1335de0754
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-14 13:31:16 -0400

    Conexión a base de datos y modelo curso

--- src/main/java/com/edutech/mscurso/model/Curso.java
+++ src/main/java/com/edutech/mscurso/model/Curso.java
@@ -1,5 +1,34 @@
 package com.edutech.mscurso.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Curso")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCurso;
+
+    private String titulo;
+
+    private String descripcion;
+
+    //private Categoria categoria;
+
+    private String precio;
+
 
 }
