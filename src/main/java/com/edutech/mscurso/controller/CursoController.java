81baf8678f17ec9da1ed964b1f5ace3be1748572
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-18 17:48:15 -0400

    últimos métodos

--- src/main/java/com/edutech/mscurso/controller/CursoController.java
+++ src/main/java/com/edutech/mscurso/controller/CursoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,6 @@ import com.edutech.mscurso.service.CursoService;
 
 @RestController
 @RequestMapping("/api/v1/cursos")
-
 public class CursoController {
 
     @Autowired
@@ -36,5 +38,45 @@ public class CursoController {
     public ResponseEntity<Curso> guardar(@RequestBody Curso curso) {
         Curso nuevoCurso = cursoService.save(curso);
         return new ResponseEntity<>(nuevoCurso, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Curso> buscar(@PathVariable int idCurso) {
+        try {
+            Curso curso = cursoService.findById(idCurso);
+            return new ResponseEntity<>(curso, HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Curso> actualizar(@PathVariable int idCurso, @RequestBody Curso curso) {
+        try {
+            Curso cur = cursoService.findById(idCurso);
+            cur.setIdCurso(idCurso);
+            cur.setTitulo(curso.getTitulo());
+            cur.setDescripcion(curso.getDescripcion());
+            cur.setCategoria(curso.getCategoria());
+            cur.setPrecio(curso.getPrecio());
+            cur.setIdProfesor(curso.getIdProfesor());
+            cur.setFechaCreacion(curso.getFechaCreacion());
+            cur.setPublicado(curso.getPublicado());
+
+            cursoService.save(cur);
+            return new ResponseEntity<>(curso, HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable int idCurso) {
+        try {
+            cursoService.delete(idCurso);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
