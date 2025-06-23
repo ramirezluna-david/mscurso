1eb24466eac66f58269ad6490447941e0effb13a
david ramirez luna <davidrrluna@gmail.com>
2025-06-23 11:22:45 -0400

    update swagger

---
+++ src/main/java/com/edutech/mscurso/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.edutech.mscurso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("EduTech Curso API")
+                        .version("1.0.0")
+                        .description("API para gestionar cursos, módulos y clases en la plataforma EduTech"));
+    }
+
+}
